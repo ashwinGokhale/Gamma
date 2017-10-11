3e8debea7669dc0579418f44f3fd01d6fc0e1835
ashwinGokhale <gokhale0@purdue.edu>
2017-10-11 02:01:29 -0400

    Added a run command

--- src/gamma.js
+++ src/gamma.js
@@ -2,12 +2,13 @@
 'use strict'
 
 import program from 'commander';
-// import omelette from 'omelette';
 import et from 'expand-tilde';
 import path from 'path';
 import chalk from 'chalk';
 import fs from 'fs';
 import glob from 'glob';
+import shell from 'shelljs';
+import rl from 'readline';
 
 // Path to the dotfile
 const dotpath = et('~/.gamma.json');
@@ -31,6 +32,17 @@ const dotpath = et('~/.gamma.json');
 // Helper functions
 const isChildOf = (child, parent) => (child !== parent) && parent.split('/').every((t, i) => child.split('/')[i] === t);
 
+const getDotfile = () => {
+	if (!fs.existsSync(dotpath)) {
+		console.log(chalk.red(`Error removing bases: ~/.gamma.json has been corrupted. Rebuilding...`));
+		let dotfile = init();
+		console.log(chalk.green(`~/.gamma.json has been rebuilt`));
+		return [dotfile, true];
+	}
+	else
+		return [require(dotpath), false];
+}
+
 const findRepos = (bases = [], dotfile = {}) => {
 	let repo_names = {};
 	bases.forEach(base => {
@@ -53,14 +65,7 @@ const findRepos = (bases = [], dotfile = {}) => {
 const getRepos = (bases = []) => {
 	// TODO: Only add bases that aren't in the dotfile
 	var repos = [];
-	let dotfile = {};
-	if (!fs.existsSync(dotpath)){
-		console.log(chalk.red(`~/.gamma.json has been corrupted. Rebuilding...`));
-		dotfile = init();
-		console.log(chalk.green(`~/.gamma.json has been rebuilt`));
-	}
-	else
-		dotfile = require(dotpath);
+	let [dotfile] = getDotfile();
 
 	if (bases.length > 0){
 		bases.forEach(base => {
@@ -86,7 +91,7 @@ const filter = (bases = []) => {
 	bases.forEach(base => {
 		base = path.resolve(et(base));
 		if(!fs.existsSync(base)){
-			console.log(chalk.red(`Base: ${base} does not exist`));
+			console.log(chalk.red(`${base} does not exist`));
 			return;
 		}
 		let dirs = base.split(path.sep);
@@ -108,25 +113,66 @@ const filter = (bases = []) => {
 	return paths;
 }
 
+const listBases = (dotfile) => Object.keys(dotfile['bases']).forEach(base => console.log(base));
+
+const listRepos = (bases = []) => getRepos(bases).forEach(base => console.log(base));
+
+const processContextBase = (base, dotfile) => {
+	// Sets the base as the context base
+	base = path.resolve(et(base));
+	if (!fs.existsSync(base))
+		return console.log(chalk.red(`${base} does not exist`));
+	
+	if (!Object.keys(dotfile['bases']).includes(base))
+		return console.log(chalk.red(`Please add: ${base} before attempting to set it to the context base`));
+	
+	dotfile['context']['base'] = base;
+
+	fs.writeFile(dotpath,JSON.stringify(dotfile, null, '\t'), 'utf8', (err) => {if(err){console.error(err)}});
+	return dotfile;
+}
+
+const processContextRepo = (repo, dotfile) => {
+	// Sets the base as the context base
+	let contextBase = dotfile['context']['base'];
+	let repoKeys = Object.keys(dotfile['bases'][contextBase]['repos']);
+	let repoIndex = repoKeys.indexOf(repo);
+	if (repoIndex < 0)
+		return console.log(chalk.red(`${repo} is not repo in the context base: ${contextBase}`));
+	
+	dotfile['context']['repo'] = {
+		name: repoKeys[repoIndex],
+		path: dotfile['bases'][contextBase]['repos'][repoKeys[repoIndex]]['path']
+	};
+
+	fs.writeFile(dotpath,JSON.stringify(dotfile, null, '\t'), 'utf8', (err) => {if(err){console.error(err)}});
+	return dotfile;
+}
+
+const runCommand = (command = '', dotfile = {}) => {
+	command = command.replace('git', '').trim();
+	let repo = dotfile['context']['repo']['path'];
+	if (!repo)
+		return console.log(chalk.red(`Context repo is missing!`));
+	command = `git -C ${repo} ${command}`;
+	let code = shell.exec(command).code;
+	if (code !== 0)
+		return console.log(chalk.red(`Command: ${command} failed!`));
+	return code;
+}
 
 // CLI commands
 const add = (base = '', bases = []) => {
 	//Adds the bases and finds all repos in it.
 	// Attempt to load the dotfile
 	let repos, repo_names = [];
-	let dotfile = {};
-	if (!fs.existsSync(dotpath)){
-		console.log(chalk.red(`~/.gamma.json has been corrupted. Rebuilding...`));
-		dotfile = init();
-		console.log(chalk.green(`~/.gamma.json has been rebuilt`));
-	}
-	else
-		dotfile = require(dotpath);
+	let [dotfile] = getDotfile();
 
 	bases = bases.concat(base);
 
 
 	// TODO: Add a spinner because glob is slow
+	console.log(chalk.yellow('Adding bases...'));
 
 	// Get the absolute path of the bases and filter/validate them
 	bases = filter(bases.map(b => path.resolve(et(b))).sort((a, b) => a.length - b.length));
@@ -174,15 +220,10 @@ const add = (base = '', bases = []) => {
 
 const remove = (base = '', bases = []) => {
 	// Removes a base, including all repos in it
-	let dotfile = {};
-	if (!fs.existsSync(dotpath)){
-		console.log(chalk.red(`~/.gamma.json has been corrupted. Rebuilding...`));
-		dotfile = init();
-		console.log(chalk.green(`~/.gamma.json has been rebuilt`));
+	let dotfile, error;
+	[dotfile, error] = getDotfile();
+	if (error)
 		return dotfile;
-	}
-	else
-		dotfile = require(dotpath);
 
 	bases.concat(base).forEach(base => {
 		let base_path = path.resolve(et(base));
@@ -201,60 +242,93 @@ const remove = (base = '', bases = []) => {
 	return dotfile;
 }
 
-
-
 const init = () => {
-	// Add the autocomplete
-	complete.setupShellInitFile();
 	// Initializes the gamma dofile and persists to disk
 	let content = {'bases': {},'context':{'base': '', 'repo': ''}};
 	fs.writeFile(dotpath,JSON.stringify(content, null, '\t'), 'utf8', (err) => {if(err){console.error(err)}});
 	return content;
 }
 
-const listRepos = (bases = []) => { getRepos(bases).forEach(base => console.log(base)); }
+const list = (bases = [], options = {}) => {
+	let [dotfile] = getDotfile();
+	if (options.bases)
+		return listBases(dotfile);
+	if (options.context)
+		return console.log(`Base: ${dotfile['context']['base']}\nRepo: ${dotfile['context']['repo']['name'] ? dotfile['context']['repo']['name'] : ''}`);
 
-const search = (base = '') => {glob.sync(`${path.resolve(et(base))}/**/.git`, {dot: true}).forEach(repo_path => console.log(repo_path.slice(0, -5)));}
-
-const listBases = () =>{
-	Object.keys(require(dotpath)['bases']).forEach(base => console.log(base));
+	return listRepos(bases);
 }
 
-program
-.command('list [bases...]')
-.alias('l')
-.description('List all git repos by base')
-.action(listRepos);
+const search = (base = '') => {
+	base = path.resolve(et(base));
+	console.log(chalk.yellow(`Searching ${base} for all repos`));
+	glob.sync(`${base}/**/.git`, {dot: true}).forEach(repo_path => console.log(repo_path.slice(0, -5)));
+}
 
-program
-.command('search <base>')
-.alias('s')
-.description('Returns the paths for all git repos in the given base')
-.action(search);
+const set = (options) => {
+	let [dotfile] = getDotfile();
+	
+	if (options.base)
+		processContextBase(options.base, dotfile);
+	if (options.repo)
+		processContextRepo(options.repo, dotfile);
+}
 
-program
-.command('init')
-.alias('i')
-.description('Initializes the gamma dotfile')
-.action(init);
+const run = () => {
+	let [dotfile, error] = getDotfile();
+	if (error)
+		return;
 
-program
-.command('add <base> [otherBases...]')
-.alias('a')
-.action(add)
-.description('Adds the specified bases as well as all git repos in them');
+	let i = rl.createInterface(process.stdin, process.stdout, null);
+	i.question("Command: ", (command = '') => {
+		runCommand(command, dotfile);
+		i.close();
+		process.stdin.destroy();
+	});
+}
 
-program
-.command('remove <base> [otherBases...]')
-.alias('r')
-.action(remove)
-.description('Removes the specified bases from Gamma');
+const initProgram = () => {
+	program
+	.command('list [bases...]').alias('l')
+	.option('-b, --bases', 'Lists the bases', false)
+	.option('-c, --context', 'Lists the current context', false)
+	.description('List all git repos by base')
+	.action(list);
+	
+	program
+	.command('search <base>').alias('s')
+	.description('Returns the paths for all git repos in the given base')
+	.action(search);
+	
+	program
+	.command('init').alias('i')
+	.description('Initializes the gamma dotfile')
+	.action(init);
+	
+	program
+	.command('add <base> [otherBases...]').alias('a')
+	.description('Adds the specified bases as well as all git repos in them')
+	.action(add);
+	
+	program
+	.command('remove <base> [otherBases...]').alias('r')
+	.description('Removes the specified bases from Gamma')
+	.action(remove);
+	
+	program
+	.command('set')
+	.option('-b, --base <base>', 'Sets the base of the context')
+	.option('-r, --repo <repo>', 'Sets the repo within the base of the context')
+	.description('Set the base or repo of the context')
+	.action(set);
 
-program
-.command('context')
-.command('list')
-.command('set')
+	program
+	.command('run')
+	.description('Runs a git command in the context')
+	.action(run);
+}
+
+initProgram();
 
 program.parse(process.argv);
 
-console.log(program.context);
